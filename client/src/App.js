9fceffed1787a9cc7913b25cd77a6d562d78eedb
Akshay Ingle <akshayingle9898@gmail.com>
2022-12-14 23:40:16 +0530

    navbar and footer added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Footer/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
