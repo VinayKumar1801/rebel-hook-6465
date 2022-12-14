5d33b037c8828c996f912153c4178e687d4655e2
Shreedhar Hegde <snhkelemane@gmail.com>
2022-12-14 21:44:41 +0530

    updated Pricing page

--- client/src/components/Pricing/Platform.jsx
+++ client/src/components/Pricing/Platform.jsx
@@ -1,25 +1,39 @@
 import { Fragment } from "react";
 import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import PlanVariants from "./PlanVariants";
+import Features from "./Features";
+import Questions from "./Questions";
 
 export default function Platform() {
   return (
     <Fragment>
       <Box>
-        <Tabs variant="enclosed" w="full">
-          <TabList>
+        <Tabs variant="enclosed">
+          <TabList
+            w="full"
+            justifyContent="space-between"
+            maxW="container.lg"
+            m="auto"
+            py="4"
+            fontSize="2xl"
+          >
             <Tab>Marketing Platform</Tab>
             <Tab>Websites & Commerce</Tab>
             <Tab>Transactional Email</Tab>
           </TabList>
           <TabPanels>
             <TabPanel>
-              <Box>One</Box>
+              <Box>
+                <PlanVariants />
+                <Features />
+                <Questions />
+              </Box>
             </TabPanel>
             <TabPanel>
-              <Box>Two</Box>
+              <Box textAlign="center">Two</Box>
             </TabPanel>
             <TabPanel>
-              <Box>Three</Box>
+              <Box textAlign="center">Three</Box>
             </TabPanel>
           </TabPanels>
         </Tabs>
--- client/src/pages/Pricing.jsx
+++ client/src/pages/Pricing.jsx
@@ -1,14 +1,9 @@
-// import Features from "../components/Pricing/Features";
-import Features from "../components/Pricing/Features";
-import PlanVariants from "../components/Pricing/PlanVariants";
 import Platform from "../components/Pricing/Platform";
 
 export default function Pricing() {
   return (
     <div>
       <Platform />
-      <PlanVariants />
-      <Features />
     </div>
   );
 }
