ffada54bd7dcd167a4f6c8a11b1109f43aadc7ee
Shreedhar Hegde <snhkelemane@gmail.com>
2022-12-14 15:16:09 +0530

    created Pricing page

---
+++ client/src/pages/Pricing.jsx
@@ -0,0 +1,11 @@
+import PlanVariants from "../components/Pricing/PlanVariants";
+import Platform from "../components/Pricing/Platform";
+
+export default function Pricing() {
+  return (
+    <div>
+      <Platform />
+      <PlanVariants />
+    </div>
+  );
+}
