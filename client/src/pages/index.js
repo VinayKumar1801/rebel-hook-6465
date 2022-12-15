1a214507a7452dce60976f596dec51efd65ea8ce
Shreedhar Hegde <snhkelemane@gmail.com>
2022-12-15 17:51:50 +0530

    created index file to export all pages and components

---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import Checkout from "./Checkout/Checkout";
+import Pricing from "./Pricing";
+import Login from "./login/Login";
+import Signup from "./signup/Signup";
+
+export { Checkout, Pricing, Login, Signup };
