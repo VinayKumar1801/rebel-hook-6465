6ae5ed7005ff732bbb979c5928c657c9479a0665
rakeshrakeshyadav <rakeshy84@yahoo.com>
2022-12-16 22:34:49 +0530

    added email

--- server/config/connect.js
+++ server/config/connect.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 
 const connection=async()=>{
     mongoose.set('strictQuery', true)
-    mongoose.connect(process.env.MONGO_CONN, { useNewUrlParser: true }).then(()=>console.log("mongo connected"))
+    mongoose.connect(`mongodb://127.0.0.1:27017`).then(()=>console.log("mongo connected"))
     .catch((e)=>console.log(e))
 }
 
