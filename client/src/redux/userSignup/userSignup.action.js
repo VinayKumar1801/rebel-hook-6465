30642dd98e1a4abde557fa2e98c579dbf6e9ed96
VinayKumar1801 <107485549+VinayKumar1801@users.noreply.github.com>
2022-12-17 17:06:31 +0530

    day-4

--- client/src/redux/userSignup/userSignup.action.js
+++ client/src/redux/userSignup/userSignup.action.js
@@ -4,10 +4,10 @@ import { USER_SIGNUP, USER_SIGNUP_ERROR } from "./userSignup.type";
 
 const mainUrl = process.env.REACT_APP_MAIN_URL;
 
-export const signupUser = (cred) => (dispatch) => {
+export const signupUser = (cred) => async(dispatch) => {
     // const navigate = useNavigate()
     console.log(cred)
-    let res = axios.post(`${mainUrl}/users/signup`, cred)
+    let res = await axios.post(`${mainUrl}/users/signup`, cred)
         .then((res) => {
             dispatch({ type: USER_SIGNUP });
         })
