1ebc713f8ab607e7e659bafdc2646bd1fcc71edd
rakeshrakeshyadav <rakeshy84@yahoo.com>
2022-12-18 01:15:48 +0530

    a

--- server/routes/email.js
+++ server/routes/email.js
@@ -29,3 +29,22 @@ sgMail.send(msg)
 });
 
 module.exports = router;
+
+
+const { MailtrapClient } = require("mailtrap");
+
+// For this example to work, you need to set up a sending domain,
+// and obtain a token that is authorized to send from the domain
+
+const client = new MailtrapClient({ token: TOKEN });
+
+const sender = { name: "Mailtrap Test", email: SENDER_EMAIL };
+
+client
+  .send({
+    from: sender,
+    to: [{ email: RECIPIENT_EMAIL }],
+    subject: "Hello from Mailtrap!",
+    text: "Welcome to Mailtrap Sending!",
+  })
+  .then(console.log, console.error);
