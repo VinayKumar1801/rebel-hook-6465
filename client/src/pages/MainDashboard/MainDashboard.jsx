7dc073e35a455fb9a09ac285877c0427a3d21dac
Shreedhar Hegde <snhkelemane@gmail.com>
2022-12-19 17:39:33 +0530

    updated dashboard

--- client/src/pages/MainDashboard/MainDashboard.jsx
+++ client/src/pages/MainDashboard/MainDashboard.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import SidebarWithHeader from '../../components/Sidebar/Sidebar'
-import {Navigate, Route, Routes} from "react-router-dom"
-import Email from '../Dashboard/Email';
-import Contact from '../Dashboard/Contact';
+import React from "react";
+import { useSelector } from "react-redux";
+import SidebarWithHeader from "../../components/Sidebar/Sidebar";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Email from "../Dashboard/Email";
+import Contact from "../Dashboard/Contact";
 
 const MainDashboard = () => {
   const { isAuth } = useSelector((store) => store.userLogin);
-    if(!isAuth){
-        return <Navigate to ="/"/>
-    }
+  if (!isAuth) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div>
-     <SidebarWithHeader/>
-     
-    
+      <SidebarWithHeader />
+      <Email />
     </div>
-  )
-}
+  );
+};
 
-export default MainDashboard
+export default MainDashboard;
